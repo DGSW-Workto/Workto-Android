af731f9224c5002a910d664244bf379015626e5a
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2020-06-15 11:02:13 +0900

    검색화면 디자인

--- presentation/src/main/java/com/example/workto_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/example/workto_android/ui/main/MainActivity.kt
@@ -4,11 +4,22 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.example.domain.result.EventObserver
 import com.example.workto_android.R
 import com.example.workto_android.databinding.ActivityMainBinding
 import com.example.workto_android.ui.BaseActivity
+import com.example.workto_android.ui.CategoryAdapter
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -20,6 +31,8 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     private val mainMenuFragment by inject<MainMenuFragment>()
 
+    private lateinit var bottomSheet: BottomSheetBehavior<*>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
@@ -28,14 +41,34 @@ class MainActivity : BaseActivity<MainViewModel>() {
                 lifecycleOwner = this@MainActivity
             }
 
+        bottomSheet = BottomSheetBehavior.from(binding.searchHolder)
+
         binding.bottomAppBar.setNavigationOnClickListener {
             mainMenuFragment.show(supportFragmentManager, mainMenuFragment.tag)
+        }
+
+        binding.recyclerviewCategory.apply {
+            layoutManager = FlexboxLayoutManager(this@MainActivity).apply {
+                flexWrap = FlexWrap.WRAP
+                (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+                justifyContent = JustifyContent.FLEX_START
+            }
+            adapter = CategoryAdapter(viewModel.categoryList, viewModel)
         }
 
         initObserver()
     }
 
     private fun initObserver() {
+
+        viewModel.bottomSheetState.observe(this, Observer {
+            bottomSheet.state = if (it)
+                BottomSheetBehavior.STATE_EXPANDED
+            else
+                BottomSheetBehavior.STATE_COLLAPSED
+
+        })
+
         viewModel.navigateToCreateTeam.observe(this, EventObserver {
 
         })
@@ -54,5 +87,25 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     }
 
+    override fun onBackPressed() {
+        if (bottomSheet.state == BottomSheetBehavior.STATE_EXPANDED)
+            bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+        else
+            super.onBackPressed()
+    }
 
+}
+
+@BindingAdapter("text", "toggle")
+fun toggleState(constraintLayout: ConstraintLayout, textView: TextView, isSelected: Boolean) {
+
+    constraintLayout.run {
+        if (isSelected) {
+            background = ContextCompat.getDrawable(context, R.drawable.search_toggle_on_background)
+            textView.setTextColor(Color.parseColor("#ffffff"))
+        } else {
+            background = ContextCompat.getDrawable(context, R.drawable.search_toggle_off_background)
+            textView.setTextColor(Color.parseColor("#000000"))
+        }
+    }
 }
--- presentation/src/main/java/com/example/workto_android/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/example/workto_android/ui/main/MainViewModel.kt
@@ -1,38 +1,62 @@
 package com.example.workto_android.ui.main
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.domain.result.Event
 import com.example.domain.user.GetUserDataUseCase
+import com.example.model.Category
 import com.example.model.UserData
 import com.example.workto_android.R
 import com.example.workto_android.ui.BaseViewModel
+import com.example.workto_android.util.CategorySelector
 import com.example.workto_android.util.UserDataManager
 
-class MainViewModel(private val getUserDataUseCase: GetUserDataUseCase) : BaseViewModel() {
+class MainViewModel(private val getUserDataUseCase: GetUserDataUseCase) : BaseViewModel(),
+    CategorySelector {
+
+    val searchWord = ObservableField("")
 
     private val _userData = MediatorLiveData<UserData>()
     val userData: LiveData<UserData>
         get() = _userData
 
+    private val _searchByTeam = MutableLiveData(true)
+    val searchByTeam: LiveData<Boolean>
+        get() = _searchByTeam
+
+    private val _searchByPost = MutableLiveData(false)
+    val searchByPost: LiveData<Boolean>
+        get() = _searchByPost
+
+
     private val _navigateToCreateTeam = MutableLiveData<Event<Unit>>()
-    val navigateToCreateTeam : LiveData<Event<Unit>>
+    val navigateToCreateTeam: LiveData<Event<Unit>>
         get() = _navigateToCreateTeam
 
     private val _navigateToWritePost = MutableLiveData<Event<Unit>>()
-    val navigateToWritePost : LiveData<Event<Unit>>
+    val navigateToWritePost: LiveData<Event<Unit>>
         get() = _navigateToWritePost
 
     private val _navigateToMyPage = MutableLiveData<Event<Unit>>()
-    val navigateToMyPage : LiveData<Event<Unit>>
+    val navigateToMyPage: LiveData<Event<Unit>>
         get() = _navigateToMyPage
 
     private val _navigateToMyPost = MutableLiveData<Event<Unit>>()
-    val navigateToMyPost : LiveData<Event<Unit>>
+    val navigateToMyPost: LiveData<Event<Unit>>
         get() = _navigateToMyPost
 
+    private val _bottomSheetState = MutableLiveData<Boolean>(false)
+    val bottomSheetState : LiveData<Boolean>
+        get() = _bottomSheetState
 
+    private val _selectedCategory = MutableLiveData<Pair<Int, ArrayList<Category>>>()
+    val selectedCategory: LiveData<Pair<Int, ArrayList<Category>>>
+        get() = _selectedCategory
+
+    private val allSelectedCategory = arrayListOf<Category>()
+    val categoryList: ArrayList<Category> = Category.values().toCollection(ArrayList())
 
     init {
 
@@ -40,7 +64,7 @@ class MainViewModel(private val getUserDataUseCase: GetUserDataUseCase) : BaseVi
 
         getUserDataUseCase.observe().onSuccess(_userData) {
             UserDataManager.userData = it.data
-            _userData.value = it.data
+            _userData.value = it.data!!
         }
 
         getUserDataUseCase.observe().onError(_error) {
@@ -48,17 +72,44 @@ class MainViewModel(private val getUserDataUseCase: GetUserDataUseCase) : BaseVi
         }
     }
 
+    fun openSearchHolder() {
+        _bottomSheetState.value = true
+    }
+
+    fun toggleSearchMode() {
+        _searchByPost.value = _searchByTeam.value
+        _searchByTeam.value = !(_searchByTeam.value as Boolean)
+    }
+
     fun navigate(mainMenu: MainMenu) {
         when (mainMenu.name) {
             "팀 생성" -> navigateTo(_navigateToCreateTeam)
-            "포스트 등록" ->  navigateTo(_navigateToWritePost)
+            "포스트 등록" -> navigateTo(_navigateToWritePost)
             "내 페이지" -> navigateTo(_navigateToMyPage)
             "내 포스트" -> navigateTo(_navigateToMyPost)
         }
     }
 
+    fun executeSearch() {
+        _bottomSheetState.value = false
+    }
+
     private fun navigateTo(navigate: MutableLiveData<Event<Unit>>) {
         navigate.value = Event(Unit)
+    }
+
+    fun clearSearchWord() {
+        searchWord.set("")
+    }
+
+    override fun selectCategory(category: Category) {
+        if (allSelectedCategory.contains(category))
+            allSelectedCategory.remove(category)
+        else
+            allSelectedCategory.add(category)
+
+        val position = categoryList.indexOf(category)
+        _selectedCategory.value = Pair(position, allSelectedCategory)
     }
 
 }
